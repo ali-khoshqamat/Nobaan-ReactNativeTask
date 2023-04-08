b5f2be518f476dc816c5efaf12a97c69aa225495
ali-khoshqamat <ali_khoshghamat@yahoo.com>
2023-04-08 11:03:49 +0330

    feat(HomeScreen): Implement Home and Profile Screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,49 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, FlatList, Image, StyleSheet, Text, View } from "react-native";
 import ProfileScreen from "./ProfileScreen";
 import SearchScreen from "./SearchScreen";
 import SettingsScreen from "./SettingsScreen";
 
+const IMAGES_DATA = [
+  {
+    id: "1",
+    imageUri: require("../assets/image1.jpg"),
+    caption: "Beautiful sunset",
+  },
+  {
+    id: "2",
+    imageUri: require("../assets/image2.jpg"),
+    caption: "Fun at the beach",
+  },
+  {
+    id: "3",
+    imageUri: require("../assets/image3.jpg"),
+    caption: "Fun at the beach",
+  },
+  {
+    id: "4",
+    imageUri: require("../assets/image4.jpg"),
+    caption: "Fun at the beach",
+  },
+];
+
 const HomeScreen = () => {
+  //   const images = useSelector((state: RootState) => state.images);
+  const renderItem = ({ item }) => (
+    <View style={styles.imageContainer}>
+      <Image source={item.imageUri} style={styles.image} />
+      <Text style={styles.caption}>{item.caption}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>This is Setting Screen!</Text>
+      <FlatList
+        data={IMAGES_DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 };
@@ -21,7 +56,26 @@ function HomeTabs() {
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Search" component={SearchScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ focused }) => (
+            <Image
+              // source={{ uri: user.profileImage }}
+              source={{ uri: require("../assets/profileImage.jpg") }}
+              style={{
+                width: 25,
+                height: 25,
+                borderRadius: 12.5,
+                borderWidth: focused ? 2 : 0,
+                borderColor: "blue",
+              }}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
@@ -31,13 +85,27 @@ export default HomeTabs;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  imageContainer: {
+    marginBottom: 16,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    resizeMode: "cover",
+    borderRadius: 5,
+  },
+  caption: {
+    marginTop: 8,
+    fontSize: 16,
+    fontWeight: "bold",
   },
   text: {
     fontSize: 20,
     fontWeight: "bold",
     color: "#000",
+    marginBottom: "20px",
   },
 });
--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,26 +1,190 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Image,
+} from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { updateUserProfile } from "../redux/user/userActions";
+// import { logout } from '../redux/user/userActions';
 
-const ProfileScreen = () => {
+const ProfileScreen: React.FC = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  const [editing, setEditing] = useState(false);
+  const [email, setEmail] = useState(user.email);
+  const [password, setPassword] = useState(user.password);
+  const [fullName, setFullName] = useState(user.fullName);
+  const navigation = useNavigation();
+
+  const saveHandler = () => {
+    dispatch(updateUserProfile({ fullName, email, password }));
+    setEditing(false);
+  };
+
+  const logoutHandler = () => {
+    // dispatch(logout());
+    navigation.navigate("Login");
+  };
+
+  const inputChangeHandler = (text) => {
+    setFullName(text);
+    dispatch(updateUserProfile({ fullName: text }));
+
+    // dispatch(setTextFieldData(e.target.value));
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>This is Pfofile Screen!</Text>
+      <View style={styles.header}>
+        <TouchableOpacity
+          onPress={() => {
+            editing ? saveHandler() : setEditing(true);
+          }}
+        >
+          <Text style={styles.editButton}>{editing ? "Save" : "Edit"}</Text>
+        </TouchableOpacity>
+        {/* <Text style={styles.title}>Profile</Text> */}
+        <TouchableOpacity onPress={logoutHandler}>
+          <Text style={styles.logoutButton}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.profileContainer}>
+        <Image
+          // source={{ uri: user.profileImage }}
+          source={{ uri: require("../assets/profileImage.jpg") }}
+          style={styles.profileImage}
+        />
+        <Text style={styles.fullName}>{user.fullName}</Text>
+      </View>
+      <View style={styles.profileInformation}>
+        <Text style={styles.text}>UserName</Text>
+        {editing ? (
+          <TextInput
+            style={styles.input}
+            value={fullName}
+            onChangeText={inputChangeHandler}
+          />
+        ) : (
+          <Text style={styles.userInformation}>{user.fullName}</Text>
+        )}
+        <Text style={styles.text}>Email</Text>
+        {editing ? (
+          <TextInput
+            style={styles.input}
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+        ) : (
+          <Text style={styles.userInformation}>{user.email}</Text>
+        )}
+        <Text style={styles.text}>Password</Text>
+        {editing ? (
+          <TextInput
+            style={styles.input}
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+        ) : (
+          <Text style={styles.userInformation}>{user.password}</Text>
+        )}
+      </View>
     </View>
   );
 };
 
-export default ProfileScreen;
+// ProfileScreen.navigationOptions = ({ navigation }) => ({
+//   headerTitle: () => (
+//     <View style={{ flexDirection: "row", alignItems: "center" }}>
+//       <Image
+//         source={{ uri: require("../../assets/image1.jpg") }}
+//         style={{
+//           width: 30,
+//           height: 30,
+//           borderRadius: 15,
+//           marginRight: 8,
+//         }}
+//       />
+//       <Text style={{ fontSize: 20, fontWeight: "bold" }}>My Profile</Text>
+//     </View>
+//   ),
+// });
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
   },
-  text: {
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  editButton: {
+    fontSize: 18,
+    color: "blue",
+  },
+  logoutButton: {
+    fontSize: 18,
+    color: "red",
+  },
+  emptyText: {
+    width: 60,
+  },
+  profileContainer: {
+    alignItems: "center",
+    marginBottom: 16,
+  },
+  profileInformation: {
+    alignItems: "center",
+  },
+  profileImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    marginBottom: 16,
+  },
+  input: {
+    width: "100%",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 16,
+  },
+  userInformation: {
+    width: "100%",
+    height: 40,
     fontSize: 20,
     fontWeight: "bold",
-    color: "#000",
+    // paddingHorizontal: 16,
+    marginBottom: 16,
+  },
+  profileImageHeader: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginRight: 8,
+  },
+  fullName: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  text: {
+    width: "100%",
+    height: 20,
   },
 });
+
+export default ProfileScreen;
